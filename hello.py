402ba0c1ee6f87faa457bd7ccefe1eb253871b9c
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-24 01:14:46 -0500

    String methods

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-first = 'Jhon'
-last = 'Smith'
-message = first+' ['+last+'] is a coder'
-msg = f'{first} [{last}] is a coder' # this is formatted string 
-print(message)
-print(msg)
+text = 'Python course for Beginner '
+print(len(text)) #counting string number 
+print(text.upper()) # converting to upper case
 
+print(text.lower()) #converting to lower case
 
+print(text.find('o')) # return character index
+                      # if does not exist, return -1
+print(text.replace('Beginner', 'Asolute Beginner'))
+
+print('Python' in text) #return boolean value
