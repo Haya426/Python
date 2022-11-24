c702e5992cf4016370a2079cb0d2028808af3d6e
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-23 23:32:50 -0500

    Type Conversion

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
-name = input('What is your name? ')
-favourite_color = input('what is your favourite color ')
-print(name + ' likes '+ favourite_color )
+birth_year = input('enter your birth year')
+print(type(birth_year))
+age = 2022 - int(birth_year) # type conversion from string to number
+print(type(age))
+print(age)
+
+weight_lbs = input('Weight in lbs: ')
+weight_kg = float(weight_lbs) * 0.45
+print(weight_kg)
+
 
