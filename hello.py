f77ad644bcef9f56c60cab611d6d40b3ae72ee7d
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-24 00:02:29 -0500

    Formatted String

--- hello.py
+++ hello.py
@@ -1,26 +1,8 @@
-course1 = 'Python course for "beginner"'
-course2 = "Python course for 'intermediate"
-print(course1,course2)
+first = 'Jhon'
+last = 'Smith'
+message = first+' ['+last+'] is a coder'
+msg = f'{first} [{last}] is a coder' # this is formatted string 
+print(message)
+print(msg)
 
-#multiple lines strings
-course = '''
-Hi Jhon,
-Welcome to our Python course,
-Here is our email address,
-Thank you for your support
-'''
-print(course)
-
-#indexing strings
-index = 'Python course for beginner'
-print(index[1:-1]) #removing first and last character 
-print(index[0]) #fisrt character 
-print(index[-1]) #last character
-print(index[0:3]) #return Pyt 
-print(index[1:]) # return ython for beginner
-print(index[:5]) # return Pytho
-
-#copy strings
-another = index[:]
-print(another)
 
