535690c12203d5e83971135187858ced9758d13d
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-25 11:36:13 -0500

    Comparison Operator

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-has_good_income = True
-has_good_credit = False
-has_criminal_records = False
-
-if has_good_income and has_good_credit:
-    print("Eligible payments")
-elif has_good_credit or not has_good_credit:
-    print("Eligible payments again")
+name ="Ram"
+name_count = len(name)
+if name_count <3:
+    print("your name must be at least 3 characters")
+elif name_count > 10:
+    print("your name must be at maximum 10 characters")
+else:
+    print("Name looks good")
