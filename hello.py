6bbd1cd17019077c492488eab218e187b090af17
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-27 03:13:46 -0500

    List Methods

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-matrix [0][0] = 10
-for row in matrix:
-    for col in row:
-        print(col)
+#list methods
+numbers = [1,4,5,7,8,3,2]
+numbers.append(6) #append
+numbers.insert(0,10) #insert
+numbers.remove(10) #remove
+# numbers.clear() #clear all
+numbers.pop() #remove last item
+numbers.sort() #sort ascending order
+numbers.reverse() #sort descending order
+
+print(numbers.index(2)) #return index 
+print(50 in numbers) # return boolean value
+
+numbers2 = numbers.copy() #copy list to another list
+numbers.append(10)
+print(numbers)
+print(numbers2)
