f72d20919616b9c51c26de2b0ddaf83419e19b97
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-27 02:40:16 -0500

    Charater F using for loop

--- hello.py
+++ hello.py
@@ -1,11 +1,6 @@
-for item in 'Python':
-    print(item)
-
-for item in ['Mosh', 'Jhon', 'Sarah', 'David']:
-    print(item)
-    
-for item in [1,2,3,4,5,6,7,8,9]:
-    print(item)
-
-for item in range(0,10,2): #second arg - lens
-    print(item)            #third arg - step
+numbers = [5,2,5,2,2]
+for x in numbers:
+    output = ""
+    for y in range(x):
+        output +='*'
+    print(output)
