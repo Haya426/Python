3ef0eaa84c2b981386571da1c8de80b961941208
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-27 02:52:35 -0500

    list

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-numbers = [5,2,5,2,2]
-for x in numbers:
-    output = ""
-    for y in range(x):
-        output +='*'
-    print(output)
+#list 
+names = ['Jhon', 'Marry', 'David', 'Sarah', 'Peter']
+print(names[:])  #return all list names
+print(names[1:]) #start from Marry
+print(names[1:4])
+print(names[-1]) #last index
+names[0] = "Jhone"
+print(names) #update the list of names
