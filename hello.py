04f70af500507380d4f4d2b4277203ff9b5ea5b8
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-27 02:59:36 -0500

    2D Lists

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
-#list 
-names = ['Jhon', 'Marry', 'David', 'Sarah', 'Peter']
-print(names[:])  #return all list names
-print(names[1:]) #start from Marry
-print(names[1:4])
-print(names[-1]) #last index
-names[0] = "Jhone"
-print(names) #update the list of names
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+matrix [0][0] = 10
+for row in matrix:
+    for col in row:
+        print(col)
