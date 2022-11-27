69ac0288020f18086b4ecdc3d7be08843cbd8877
MB Phong Ram <mabuphongram171@gmail.com>
2022-11-27 03:18:25 -0500

    Remove duplicate lists

--- hello.py
+++ hello.py
@@ -1,17 +1,6 @@
-#list methods
-numbers = [1,4,5,7,8,3,2]
-numbers.append(6) #append
-numbers.insert(0,10) #insert
-numbers.remove(10) #remove
-# numbers.clear() #clear all
-numbers.pop() #remove last item
-numbers.sort() #sort ascending order
-numbers.reverse() #sort descending order
-
-print(numbers.index(2)) #return index 
-print(50 in numbers) # return boolean value
-
-numbers2 = numbers.copy() #copy list to another list
-numbers.append(10)
-print(numbers)
-print(numbers2)
+numbers = [2,2,3,4,5,6,6,7]
+result = []
+for i in numbers:
+    if i not in result:
+        result.append(i)
+print(result)
